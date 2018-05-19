0da2772fd1f672d319427f191362cd70e8effd40
league <league@wts-im-17.attlocal.net>
2018-05-19 12:32:38 -0700

    Day 1

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,6 +1,9 @@
 package section2;
 
 import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
@@ -8,8 +11,16 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	System.out.println("Hello");
+	Robot arnold=new Robot();
+	arnold.setPenColor(color);
+	arnold.penDown();
+	arnold.setSpeed(100);
+	arnold.move(99);
+	arnold.turn(90);
+	arnold.spir
 	
-		
-		
+	
+	
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,25 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot Sam = new Robot();
 		// 5. Set your robot's pen down 
-		
+		Sam.penDown();
 		// 3. Set the robot to go at max speed (10)
-		
+		Sam.setSpeed(10);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		for (int i = 0; i < 75; i++) {
+			Sam.move(100);
+			Sam.turn(360/7);
 		
 			// 7. Change the pen color to random
-		
+		Sam.setRandomPenColor();
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-			
+			Sam.move(5 *i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		Sam.turn(360/7);
 			// 8. Set the pen width to i
-			
+		
+		}
+		
 	}
 }
